4cbc80e192fe6731db88ea62e66f01c1bed94909
Jens Nilsson <jenszza@gmail.com>
2025-02-12 04:49:23 +0100

    Solve day 10, part 2

--- apps/day10.cpp
+++ apps/day10.cpp
@@ -1,5 +1,7 @@
-#include <iomanip>
+#include <iterator>
 #include <list>
+#include <map>
+#include <numeric>
 #include <vector>
 #include "aoc/utils.hpp"
 
@@ -37,15 +39,16 @@ bool find_solution(std::list<number_t>& solution,
   return false;
 }
 
+number_t get_my_rating(std::vector<number_t> const& input) {
+  return *std::max_element(input.begin(), input.end()) + 3;
+}
+
 void part_1(std::vector<number_t> const& input) {
-  auto const my_rating = *std::max_element(input.begin(), input.end()) + 3;
-  std::list<number_t> solution;
-  std::vector<number_t> remaining{input.begin(), input.end()};
+  auto const my_rating = get_my_rating(input);
 
-  auto has_solution = find_solution(solution, remaining);
-  assert(has_solution);
-
+  std::vector<number_t> solution = input;
   solution.push_back(my_rating);
+  std::sort(solution.begin(), solution.end());
 
   number_t prev = 0;
   std::pair<number_t, number_t> diffs{0, 0};
@@ -60,6 +63,44 @@ void part_1(std::vector<number_t> const& input) {
   std::cout << "Part 1: " << diffs.first * diffs.second << std::endl;
 }
 
+number_t get_path_count(std::vector<number_t>::const_iterator const start_node,
+                        std::vector<number_t>::const_iterator const end,
+                        std::map<number_t, number_t>& cache) {
+  if (start_node + 1 == end) {
+    return 1;
+  }
+  if (auto const search = cache.find(*start_node); search != cache.end()) {
+    return search->second;
+  }
+  std::vector<number_t> next_nodes{};
+  std::copy_if(start_node + 1, end, std::back_inserter(next_nodes),
+               [&start_node](auto num) {
+                 return (num >= *start_node + 1) && (num <= *start_node + 3);
+               });
+  assert(next_nodes.size() > 0);
+  number_t count = 0;
+  for (auto it = next_nodes.cbegin(); it != next_nodes.cend(); it++) {
+    auto start = std::find(start_node, end, *it);
+    count += get_path_count(start, end, cache);
+  }
+  cache[*start_node] = count;
+  return count;
+}
+
+void part_2(std::vector<number_t> const& input) {
+  auto const my_rating = get_my_rating(input);
+
+  std::vector<number_t> solution = input;
+  solution.push_back(my_rating);
+  solution.insert(solution.begin(), 0);
+  std::sort(solution.begin(), solution.end());
+
+  std::map<number_t, number_t> cache;
+  auto count = get_path_count(solution.cbegin(), solution.cend(), cache);
+
+  std::cout << "Part 2: " << count << std::endl;
+}
+
 int main() {
   auto reader = utils::InputReader(__FILE__);
   auto input_str = reader.getlines();
@@ -68,6 +109,7 @@ int main() {
                  str_to_num);
 
   // Quicker to find a solution.
-  std::sort(input.begin(), input.end());
   part_1(input);
+
+  part_2(input);
 }
